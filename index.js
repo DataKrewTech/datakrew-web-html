119dd3889f6881e3c60d25433479ecd76f573ba5
amolbhosale11 <amol@stack-avenue.com>
2020-04-22 20:02:53 +0530

    modal submit msg changed

--- index.js
+++ index.js
@@ -462,7 +462,7 @@ window.addEventListener("DOMContentLoaded", function () {
   function success() {
     form.reset();
     button.style = "display: none ";
-    status.innerHTML = "Thanks!";
+    status.innerHTML = "Thank you for the enquiry, Our Team will reach back to you shortly...";
   }
   function error() {
     status.innerHTML = "Oops! There was a problem.";
