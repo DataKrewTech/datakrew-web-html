af113c7e093f9effd5b0e99675aea1627434fef3
amolbhosale11 <amol@stack-avenue.com>
2020-04-22 19:41:58 +0530

    common modal updated -modal click on outside issue fixed

--- index.js
+++ index.js
@@ -489,3 +489,26 @@ function ajax(method, url, data, success, error) {
   };
   xhr.send(data);
 }
+
+
+
+function modalClose() {
+  if (location.hash == '#open-modal') {
+      location.hash = '';
+  }
+}
+
+document.addEventListener('keyup', function(e) {
+  if (e.keyCode == 27) {
+      modalClose();
+  }
+});
+
+var modal = document.querySelector('#open-modal');
+modal.addEventListener('click', function(e) {
+  modalClose();
+}, false);
+
+modal.children[0].addEventListener('click', function(e) {
+  e.stopPropagation();
+}, false);
